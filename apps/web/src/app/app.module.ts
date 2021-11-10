bed96d849eb4f582b178e3f2efeb6734d498434d
Roberto Kondo <robertokondo8496@gmail.com>
2021-11-10 03:16:02 +0000

    Cria metodo getAll() no ListaLivrosService

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MenubarModule } from 'primeng/menubar';
 
@@ -18,6 +19,7 @@ import { ListaLivrosComponent } from './components/lista-livros/lista-livros.com
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MenubarModule,
   ],
--- apps/web/src/app/services/lista-livros/lista-livros.service.ts
+++ apps/web/src/app/services/lista-livros/lista-livros.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import {
+  map,
+} from 'rxjs/operators';
+
+import {
+  Livro,
+} from '../../../../../../libs/common/src/lib/models/livro';
+
+import {
+  ILivro,
+} from '../../../../../../libs/common/src/lib/interfaces/livro';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListaLivrosService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+  ) { }
+
+
+  public getAll(): Observable<Livro[]> {
+    return this.httpClient.get<ILivro[]>(`http://localhost:3333/api/livros`).pipe(
+      map((iLivros: ILivro[]) => {
+        return iLivros.map((iLivro: ILivro) => new Livro(
+          iLivro.id,
+          iLivro.imagem,
+          iLivro.titulo,
+          iLivro.descricao,
+          iLivro.url,
+        ));
+      }),
+    );
+  }
+
 }
