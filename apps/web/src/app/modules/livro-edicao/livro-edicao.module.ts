16cbbfc3f6bf2dca31b30fc0e4878d5de3dabfc0
Roberto Kondo <robertokondo8496@gmail.com>
2021-12-12 01:09:10 +0000

    Cria LivroEdicaoModule comando: `npx ng generate @schematics/angular:module --name=modules/livro-edicao --project=web --routing --no-interactive`

---
+++ apps/web/src/app/modules/livro-edicao/livro-edicao.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LivroEdicaoRoutingModule } from './livro-edicao-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LivroEdicaoRoutingModule
+  ]
+})
+export class LivroEdicaoModule { }
