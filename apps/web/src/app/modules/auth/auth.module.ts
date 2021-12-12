f5cdb01b809af120467ae3065421e1f00bf09906
Roberto Kondo <robertokondo8496@gmail.com>
2021-12-12 03:17:01 +0000

    Cria LoginComponent. - Comando:   npx ng generate @schematics/angular:component --name=modules/auth/components/login --project=web --module=modules/auth --export --no-interactive

--- apps/web/src/app/modules/auth/auth.module.ts
+++ apps/web/src/app/modules/auth/auth.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
+  ],
+  exports: [
+    LoginComponent
   ]
 })
 export class AuthModule { }
