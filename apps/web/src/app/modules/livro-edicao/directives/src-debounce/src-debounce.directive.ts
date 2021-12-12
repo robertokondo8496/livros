057d77a9ddac7bae744c6616abdfde7e5e60a1d9
Roberto Kondo <robertokondo8496@gmail.com>
2021-12-12 03:58:43 +0000

    Finaliza a implementação da diretiva customizada.

--- apps/web/src/app/modules/livro-edicao/directives/src-debounce/src-debounce.directive.ts
+++ apps/web/src/app/modules/livro-edicao/directives/src-debounce/src-debounce.directive.ts
@@ -1,16 +1,45 @@
-import { Directive, EventEmitter, HostBinding, Input, Output } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  Input,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
+
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Directive({
   selector: '[livrosSrcDebounce]'
 })
-export class SrcDebounceDirective {
+export class SrcDebounceDirective implements OnChanges, OnDestroy {
 
   @Input()
   public livrosSrcDebounce?: string;
 
-  constructor() { }
+  constructor() {
+    this.src$.pipe(
+      takeUntil(this.subUnsubscribe),
+      debounceTime(1000),
+    ).subscribe(
+      (src: string) => this.srcDebounced = src,
+    );
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.artigariaSrcDebounce) {
+      this.src$.next(this.artigariaSrcDebounce);
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.subUnsubscribe.next();
+    this.subUnsubscribe.complete();
+
+  }
   @HostBinding('attr.src')
   public get src(): string {
-    return this.livrosSrcDebounce || '';
+    return this.srcDebounced;
   }
 }
