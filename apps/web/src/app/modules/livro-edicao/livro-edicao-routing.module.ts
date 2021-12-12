9de0dee35c23f18d2a834e45d74d0b949e905bf0
Roberto Kondo <robertokondo8496@gmail.com>
2021-12-12 01:21:56 +0000

    Modulo de edição de artigos

--- apps/web/src/app/modules/livro-edicao/livro-edicao-routing.module.ts
+++ apps/web/src/app/modules/livro-edicao/livro-edicao-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LivroComponent } from './components/livro/livro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ':id',
+    component: LivroComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
